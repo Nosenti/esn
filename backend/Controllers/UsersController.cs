6f507d23526684de30b5bb47b81560eed4a74ecd
Innocent <iingabir@andrew.cmu.edu>
2025-03-04 12:28:53 +0200

    setup folders

---
+++ backend/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
