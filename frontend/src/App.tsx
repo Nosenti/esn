cf77c6d8a19173437726727079cc66c8287f5f30
nosenti <inno100os@gmail.com>
2025-04-02 14:10:04 +0200

    new changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
       <Box>
-        <Typography variant='h1'>Hello World</Typography>
+        <Typography variant='h1'>Hello World! new updates</Typography>
       </Box>
     </div>
   )
